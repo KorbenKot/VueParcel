ca460c13a5e7665af56b5e9ab038ed3e0819232a
Alexander <rober.fmc@gmail.com>
2019-11-23 00:19:23 +0300

    proj init

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>About Page</h1>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, incidunt?
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'About',
+        mounted: function () {
+            console.log('about mounted!');
+        }
+    }
+</script>
